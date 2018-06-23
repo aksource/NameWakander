79bcf52c5e711f9a745737a68a7fe45aa8603df6
aksource <akira.mew@gmail.com>
2018-06-23 09:58:58 +0900

    1.12-1.3.5
    - DimensionIDの取得方法を変更

--- java/namewakander/DimensionListBuilder.java
+++ java/namewakander/DimensionListBuilder.java
@@ -2,6 +2,7 @@ package namewakander;
 
 import com.google.common.collect.Lists;
 import net.minecraft.util.text.translation.I18n;
+import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
 import net.minecraftforge.common.DimensionManager;
 
@@ -17,11 +18,13 @@ public class DimensionListBuilder extends ObjectListBuilder {
     void create() {
         String str;
         WorldProvider provider;
-        for (int i : DimensionManager.getStaticDimensionIDs()) {
-            provider = DimensionManager.createProviderFor(i);
-            if (provider != null) {
-                str = String.format("%s, %s", provider.getDimensionType().getName(), I18n.translateToLocal(provider.getDimensionType().getName()));
-                dimensionIdList.add(new IdNameObj<>(i, str));
+        for (DimensionType type : DimensionType.values()) {
+            for (int i : DimensionManager.getDimensions(type)) {
+                provider = DimensionManager.createProviderFor(i);
+                if (provider != null) {
+                    str = String.format("%s, %s", provider.getDimensionType().getName(), I18n.translateToLocal(provider.getDimensionType().getName()));
+                    dimensionIdList.add(new IdNameObj<>(i, str));
+                }
             }
         }
     }
